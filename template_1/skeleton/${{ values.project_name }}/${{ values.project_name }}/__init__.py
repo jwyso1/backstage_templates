e581fbc999800be7e1c0386b1f51c8d785ef49f8
jwysokinski <v-jwysokinski@expediagroup.com>
2023-10-18 10:51:16 +0200

    template 1 fixing github actions render

--- template_1/skeleton/${{ values.project_name }}/${{ values.project_name }}/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Top-level package for {{ values.project_name }}."""
-
-__author__ = """{{ values.owner }}"""
-__version__ = '{{ values.version }}'
--- template_1/skeleton/docs/conf.py
+++ template_1/skeleton/docs/conf.py
@@ -19,7 +19,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../${{ values.project_name }}'))
 
 import {{ values.project_slug }}
 

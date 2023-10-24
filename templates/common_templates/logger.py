8c8b6c4571dd7b2d70ff20635ae5d6cbf06a8b21
jwysokinski <v-jwysokinski@expediagroup.com>
2023-10-24 15:08:30 +0200

    template 1, tools usage imprv

--- templates/common_templates/logger.py
+++ templates/common_templates/logger.py
@@ -1,16 +1,17 @@
 import logging
-import tools.constant.logging as t_logger
+import tools.constants.logging as t_logger
 
 logger = logging.getLogger(__name__)
+
 
 class Logger:
     def __init__(self) -> None:
         self.create_stream_handler()
     
-    def create_stream_handler(self, log_level=logging.WARNING, stream=None): # todo SS: skip typehint check
+    def create_stream_handler(self, log_level: int = logging.WARNING, stream=None):  # todo SS: skip typehint check
         handler = logging.StreamHandler(stream)
         handler.setLevel(log_level)
-        format = logging.Formatter(t_logger.default_formatter)
+        format = logging.Formatter(t_logger.DEFAULT_FORMATTER)
         handler.setFormatter(format)
 
 

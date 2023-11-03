f94debe41efa56008061631f5d120b7efc05837e
jwysokinski <v-jwysokinski@expediagroup.com>
2023-11-03 12:17:21 +0100

    poetry install

--- templates/run_poetry_update.py
+++ templates/run_poetry_update.py
@@ -1,7 +1,11 @@
 import subprocess
 
 grep_process = subprocess.Popen(
-    ["python", "-m", "poetry", "update", "--lock"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True, cwd="template_1"
+    ["python", "-m", "poetry", "update", "--lock"],
+    stdin=subprocess.PIPE,
+    stdout=subprocess.PIPE,
+    shell=True,
+    cwd="./template_1"
 )
 for line in grep_process.stdout:
     print(line.decode("utf-8").strip())

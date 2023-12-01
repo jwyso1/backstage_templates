371dffbf4a0f4740ab692a4b0e300139509d7dcb
jwysokinski <v-jwysokinski@expediagroup.com>
2023-12-01 11:20:49 +0100

    poetry template1

--- templates/run_poetry_update.py
+++ templates/run_poetry_update.py
@@ -1,12 +1,11 @@
 import subprocess
 import os
 dir_path = os.path.dirname(os.path.realpath(__file__))
-grep_process = subprocess.Popen(
+print("hello")
+process = subprocess.Popen(
     ["python", "-m", "poetry", "update", "--lock"],
     stdin=subprocess.PIPE,
     stdout=subprocess.PIPE,
     shell=True,
     cwd=dir_path + "/template_1"
 )
-# for line in grep_process.stdout:
-#     print(line.decode("utf-8").strip())
